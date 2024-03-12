16ee20573e03293f909099f7d98652f7870f9b7d
AEC-業務改革室 <75456370+AEC-Gyoumu-Department@users.noreply.github.com>
2024-03-12 14:43:07 +0900

    Update usbcam_line_tracer_hsv.py

--- usbcam_line_tracer_hsv.py
+++ usbcam_line_tracer_hsv.py
@@ -555,10 +555,10 @@ if __name__ == '__main__':
                     reset_pid_params()
                     isPausingLinetrace = True # ライントレース一時停止
                     twd.enable() # ラインロストで disable 状態になっている場合がある
-                    twd.free(0.5) # 停止、タイムアウト0.5秒
+                    twd.free(1) # 停止、タイムアウト0.5秒
                     
                     # その場で 180°旋回する
-                    twd.pivot_turn(20, 180, 10) # TWD初期化時、tread を正確に設定していない場合、ズレる
+                    #twd.pivot_turn(20, 180, 10) # TWD初期化時、tread を正確に設定していない場合、ズレる
 
                     # (ア) 搬送ローラーで荷物の搬送を行う場合
                     # twd.move_straight(15, 300, 5.5)
