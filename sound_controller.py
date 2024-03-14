c6f895d0438e122505ccd708bafb8ebf6ebec2ea
AEC-業務改革室 <75456370+AEC-Gyoumu-Department@users.noreply.github.com>
2024-03-14 18:10:05 +0900

    Update sound_controller.py
    Try this new version of def beep, because the early version launch a error

--- sound_controller.py
+++ sound_controller.py
@@ -9,14 +9,27 @@ class BeepPlayer:
         self.thread = None
         self.current_beep = ""
 
-    def beep(self, frequency, duration):
-        # Cria um array numpy para representar o som de um beep
-        t = np.linspace(0, duration, int(44100 * duration), False)
-        beep = np.sin(frequency * t * 2 * np.pi)
-        beep = np.int16(beep * 32767)
-
-        # Reproduz o som
-        play_obj = sa.play_buffer(beep, 1, 2, 44100)
+    def beep(self, tmp_frequency, tmp_duration):
+        
+        frequency = tmp_frequency  # Our played note will be 440 Hz
+        fs = 44100  # 44100 samples per second
+        seconds = tmp_duration  # Note duration of 3 seconds
+        
+        # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+        t = np.linspace(0, seconds, seconds * fs, False)
+        
+        # Generate a 440 Hz sine wave
+        note = np.sin(frequency * t * 2 * np.pi)
+        
+        # Ensure that highest value is in 16-bit range
+        audio = note * (2**15 - 1) / np.max(np.abs(note))
+        # Convert to 16-bit data
+        audio = audio.astype(np.int16)
+        
+        # Start playback
+        play_obj = sa.play_buffer(audio, 1, 2, fs)
+        
+        # Wait for playback to finish before exiting
         play_obj.wait_done()
 
     def play_beep_intermittent(self, frequency, duration, interval):
