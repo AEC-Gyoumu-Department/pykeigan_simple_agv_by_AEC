2d90ec449c073baf10b99ff8132ad78a2315ede5
AEC-業務改革室 <75456370+AEC-Gyoumu-Department@users.noreply.github.com>
2024-03-14 18:29:08 +0900

    Update sound_controller.py
    The early version of this class is full changed. The  beep reprodution error is not easy fixed.
    The new version use a mp3 files

--- sound_controller.py
+++ sound_controller.py
@@ -4,11 +4,23 @@ import numpy as np
 import time
 
 class SoundPlayer(threading.Thread):
-    def __init__(self, file_path):
+    def __init__(self):
         super().__init__()
-        self.file_path = file_path
+        self.file_path = r"/sound/normal_move.mp3" 
+        self.normal_move_sound =  r"/sound/normal_move.mp3"
+        self.blocked_move_sound = r"/sound/blocked_move.mp3"
+        self.no_sound = ""
         self._stop_event = threading.Event()
 
+    def set_no_sound(self):
+        self.file_path = self.no_sound
+
+    def set_normal_move_sound():
+        self.file_path = self.normal_move_sound
+
+    def set_blocked_move_sound():
+         self.file_path =  self.blocked_move_sound
+    
     def stop(self):
         self._stop_event.set()
 
@@ -22,9 +34,10 @@ class SoundPlayer(threading.Thread):
             play_obj.wait_done()
 
     def play(self):
-        wave_obj = sa.WaveObject.from_wave_file(self.file_path)
-        play_obj = wave_obj.play()
-        play_obj.wait_done()
+        if self.file_path != "":
+            wave_obj = sa.WaveObject.from_wave_file(self.file_path)
+            play_obj = wave_obj.play()
+            play_obj.wait_done()
 
     def play_loop(self):
         while not self._stop_event.is_set():
