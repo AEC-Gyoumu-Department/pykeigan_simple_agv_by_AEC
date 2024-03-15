be8b96c3274713f5a323dd100bdb467971973381
AEC-業務改革室 <75456370+AEC-Gyoumu-Department@users.noreply.github.com>
2024-03-15 11:44:20 +0900

    Update sound_controller.py

--- sound_controller.py
+++ sound_controller.py
@@ -6,32 +6,35 @@ import time
 class SoundPlayer(threading.Thread):
     def __init__(self):
         super().__init__()
-        self.file_path = r"/sound/normal_move.mp3" 
-        self.normal_move_sound =  r"/sound/normal_move.mp3"
-        self.blocked_move_sound = r"/sound/blocked_move.mp3"
+        self.file_path = ""
+        self.normal_move_sound =  r"sound/normal_move.wav"
+        self.blocked_move_sound = r"sound/blocked_move.wav"
         self.no_sound = ""
         self._stop_event = threading.Event()
 
     def set_no_sound(self):
         self.file_path = self.no_sound
 
-    def set_normal_move_sound():
+    def set_normal_move_sound(self):
         self.file_path = self.normal_move_sound
 
-    def set_blocked_move_sound():
+    def set_blocked_move_sound(self):
          self.file_path =  self.blocked_move_sound
-    
+
     def stop(self):
         self._stop_event.set()
 
     def run(self):
-        wave_obj = sa.WaveObject.from_wave_file(self.file_path)
-        play_obj = wave_obj.play()
-        play_obj.wait_done()
-
-        while not self._stop_event.is_set():
+        if self.file_path != "":
+            wave_obj = sa.WaveObject.from_wave_file(self.file_path)
             play_obj = wave_obj.play()
             play_obj.wait_done()
+
+        while not self._stop_event.is_set():
+            if self.file_path != "":
+                wave_obj = sa.WaveObject.from_wave_file(self.file_path)
+                play_obj = wave_obj.play()
+                play_obj.wait_done()
 
     def play(self):
         if self.file_path != "":
